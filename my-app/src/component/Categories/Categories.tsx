b86cd21957b577841feb194cec2332e7c3c7c125
Zinny Edmund <edmunddebby24@gmail.com>
2025-07-03 12:02:31 -0700

    Changed some images and also worked on the categories section

--- my-app/src/component/Categories/Categories.tsx
+++ my-app/src/component/Categories/Categories.tsx
@@ -2,30 +2,27 @@ import { useState  } from 'react'
 import { MdArrowBackIos } from "react-icons/md";
 import { MdArrowForwardIos } from "react-icons/md";
 import './Categories.css'
-import straw from '../../Images/straw.webp'
-import KO  from '../../Images/KO.webp'
-import ginnyandgeorgina from '../../Images/ginnyandgeorgina.jpg'
-import exterritorial from '../../Images/exterritorial.jpg'
+import Squid from '../../Images/Squid.jpg'
+import Straw from '../../Images/Straw.jpg'
+import untildawn  from '../../Images/untildawn.webp'
+import ginny from '../../Images/ginny.webp'
+import territorial from '../../Images/territorial.jpg'
 import lovestory from '../../Images/lovestory.jpg'
-import Havoc from '../../Images/Havoc.jpg'
-import sikandar from '../../Images/sikandar.jpg'
-import dear from '../../Images/dear.jpg'
-import lastbullet from '../../Images/lastbullet.jpg'
+import havoc from '../../Images/havoc.jpg'
+import sandman from '../../Images/sandman.jpg'
+import stranger from '../../Images/stranger.jpg'
+import kraven from '../../Images/kraven.jpg'
 import wednesday from '../../Images/wednesday.jpg'
 
 
 const Categories = () => {
   const [position, setPosition] = useState(0);
   const visibleSlides = 5;
-  const slideWidth = 260; // 290px + 10px margin
-  const images = [straw, KO, ginnyandgeorgina, exterritorial, lovestory, Havoc, sikandar, dear, lastbullet, wednesday, ];
+  const slideWidth = 185; // Margin
+  const images = [Squid, Straw, untildawn, ginny, territorial, lovestory, havoc, sandman, stranger, kraven, wednesday, ];
   // Calculate the maximum position based on the number of images and visible slides
   // If there are fewer images than visible slides, maxPosition will be 0
   const maxPosition = images.length - visibleSlides;
-
-  
-  
-
   const moveSlide = (direction: number) => {
     let newPosition = position + direction;
 
@@ -37,26 +34,31 @@ const Categories = () => {
   };
 
   return (
-    <div className="slider-container">
-      <button className="slider-btn left" onClick={() => moveSlide(-5)}>
-        <MdArrowBackIos color='white' size={50} />
-      </button>
-
-      <div className="slider">
-        <div
-          className="slide-track"
-          style={{ transform: `translateX(${-position * slideWidth}px)` }}
-        >
-          {images.map((src, index) => (
-            <img key={index} src={src} alt={`Slide ${index + 5}`} className="slide" />
-          ))}
-        </div>
+    <section>
+      <div className='trending-header'>
+        <h1>Trending Now</h1>
       </div>
+      <div className="slider-container">
+        <button className="slider-btn left" onClick={() => moveSlide(-2)}>
+          <MdArrowBackIos color='white' size={20} />
+        </button>
 
-      <button className="slider-btn right" onClick={() => moveSlide(1)}>
-        <MdArrowForwardIos color='white' size={50} />
-      </button>
-    </div>
+        <div className="slider">
+          <div
+            className="slide-track"
+            style={{ transform: `translateX(${-position * slideWidth}px)` }}
+          >
+            {images.map((src, index) => (
+              <img key={index} src={src} alt={`Slide ${index + 1}`} className="slide" />
+            ))}
+          </div>
+        </div>
+
+        <button className="slider-btn right" onClick={() => moveSlide(2)}>
+          <MdArrowForwardIos color='white' size={20} />
+        </button>
+      </div>
+    </section>
   );
 };
 
