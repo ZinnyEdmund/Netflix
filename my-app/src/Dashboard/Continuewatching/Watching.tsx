2dee86cb0deb8f05ac777bdb284f8e58a333f22d
Zinny Edmund <edmunddebby24@gmail.com>
2025-08-01 13:01:36 -0700

    Started working on the continue section.

--- my-app/src/Dashboard/Continuewatching/Watching.tsx
+++ my-app/src/Dashboard/Continuewatching/Watching.tsx
@@ -1,10 +1,50 @@
+import "./watching.css";
+import { useEffect, useState } from "react";
+import { fetchPopularMovies } from "../../Tmdb";
 
-function Watching() {
+const TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+type Item = {
+  id: string;
+  title: string;
+  // overview: string;
+  poster_path: string;
+};
+
+const PopularMovies = () => {
+  const [movies, setMovies] = useState<Item[]>([]);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      const popular = await fetchPopularMovies();
+      setMovies(popular);
+    };
+    getMovies();
+  }, []);
+
   return (
-    <div>
+    <section>
+      <div className="De_movieheader">
+        <h1>Continue watching</h1>
+      </div>
+      <div className="movies-container">
+        {movies.length === 0 ? (
+          <p>No movies found.</p>
+        ) : (
+          movies.map((movie) => (
+            <div key={movie.id} className="movie-card">
+              <img
+                src={`${TMDB_IMAGE_URL}${movie.poster_path}`}
+                alt={movie.title}
+                style={{ width: "200px" }}
+              />
+              <h3>{movie.title}</h3>
+              {/* <p>{movie.overview.slice(0, 50)}</p> */}
+            </div>
+          ))
+        )}
+      </div>
+    </section>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Watching
+export default PopularMovies;
---
+++ my-app/src/Tmdb.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+export const fetchPopularMovies = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    return response.data.results;
+  } catch (error) {
+     if (error instanceof Error) {
+      console.error("❌ Error fetching popular movies:", error.message);
+    } else {
+      console.error("❌ Error fetching popular movies:", error);
+    }
+    return [];
+  }
+};
